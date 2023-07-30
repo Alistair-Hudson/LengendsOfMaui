01df25ea2f30e21739099239e5a0fcac7ffea8ad
AlistairHudson <alistairhudson64@gmail.com>
2023-07-30 12:03:49 +0300

    PlayerStateMachine updated to handke Targeting, Animations and movement

--- LegendsOfMaui/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ LegendsOfMaui/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -1,3 +1,4 @@
+using AlictronicGames.LegendsOfMaui.Combat.Targeting;
 using AlictronicGames.LegendsOfMaui.Controls;
 using System;
 using System.Collections;
@@ -6,20 +7,38 @@ using UnityEngine;
 
 namespace AlictronicGames.LegendsOfMaui.StateMachines.Player
 {
-    [RequireComponent(typeof(InputReader))]
+    [RequireComponent(typeof(InputReader), typeof(CharacterController), typeof(ForceReceiver))]
     public class PlayerStateMachine : StateMachine
     {
+        [field: SerializeField]
+        public float FreeLookMoveSpeed { get; private set; } = 6f;
+        [field: SerializeField]
+        public float TargetingMoveSpeed { get; private set; } = 6f;
+
         public InputReader InputReader { get; private set; } = null;
+        public CharacterController CharacterController { get; private set; } = null;
+        public Animator Animator { get; private set; } = null;
+        public Transform MainCameraTransform { get; private set; } = null;
+        public Targeter Targeter { get; private set; } = null;
+        public ForceReceiver ForceReceiver { get; private set; } = null;
 
         private void Awake()
         {
             InputReader = GetComponent<InputReader>();
+            CharacterController = GetComponent<CharacterController>();
+            ForceReceiver = GetComponent<ForceReceiver>();
+            Animator = GetComponentInChildren<Animator>();
+            Targeter = GetComponentInChildren<Targeter>();
         }
 
         private void Start()
         {
+            MainCameraTransform = Camera.main.transform;
+
             InputReader.JumpEvent += HandleOnJump;
             InputReader.DodgeEvent += HandleOnDodge;
+
+            SwitchState(new PlayerFreeLookState(this));
         }
 
         private void OnDestroy()
