d64b4e180d80bc70686b08dd082adf992abd129c
AlistairHudson <alistairhudson64@gmail.com>
2023-07-31 19:11:24 +0300

    Added ability to have various forces act on the the ForReceiver

--- LegendsOfMaui/Assets/Scripts/ForceReceiver.cs
+++ LegendsOfMaui/Assets/Scripts/ForceReceiver.cs
@@ -7,11 +7,17 @@ namespace AlictronicGames.LegendsOfMaui
     [RequireComponent(typeof(CharacterController))]
     public class ForceReceiver : MonoBehaviour
     {
+        [SerializeField]
+        private float _drag = 0.3f;
+
         private CharacterController _controller = null;
+
+        private Vector3 _impact = Vector3.zero;
+        private Vector3 _dampingVelocity = Vector3.zero;
 
         private float _vertVelocity = 0;
 
-        public Vector3 Movement => Vector3.up * _vertVelocity;
+        public Vector3 Movement => _impact + Vector3.up * _vertVelocity;
 
         private void Awake()
         {
@@ -28,6 +34,13 @@ namespace AlictronicGames.LegendsOfMaui
             {
                 _vertVelocity += Physics.gravity.y * Time.deltaTime;
             }
+
+            _impact = Vector3.SmoothDamp(_impact, Vector3.zero, ref _dampingVelocity, _drag);
+        }
+
+        public void AddForce(Vector3 force)
+        {
+            _impact += force;
         }
     }
 }
