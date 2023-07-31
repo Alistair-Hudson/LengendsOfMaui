348b9cf06ced915ba3f1f1f0528fe5cc05bc2c5f
AlistairHudson <alistairhudson64@gmail.com>
2023-07-31 19:07:00 +0300

    AnimationCallReceiver to handle animation events

---
+++ LegendsOfMaui/Assets/Scripts/AnimationCallReceiver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlictronicGames.LegendsOfMaui
+{
+
+    public class AnimationCallReceiver : MonoBehaviour
+    {
+        public void FootR()
+        {
+
+        }
+
+        public void FootL()
+        {
+
+        }
+
+        public void Hit()
+        {
+
+        }
+    }
+}
