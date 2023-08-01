f5ec648f0265e43b8c2a97dcb08b64344e75c530
AlistairHudson <alistairhudson64@gmail.com>
2023-08-01 14:25:59 +0300

    Created impact states

---
+++ LegendsOfMaui/Assets/Scripts/StateMachines/Enemies/States/EnemyImpactState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlictronicGames.LegendsOfMaui.StateMachines.Enemy
+{
+    public class EnemyImpactState : EnemyBaseState
+    {
+        private readonly int IMPACT = Animator.StringToHash("Unarmed-GetHit-F2");
+        private const float ANIMATOR_DAMP_TIME = 0.1f;
+
+        private float _duration = 1f;
+
+        public EnemyImpactState(EnemyStateMachine enemyStateMachine) : base(enemyStateMachine)
+        {
+        }
+
+        public override void Enter()
+        {
+            stateMachine.Animator.CrossFadeInFixedTime(IMPACT, ANIMATOR_DAMP_TIME);
+        }
+
+        public override void Exit()
+        {
+            
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            Move(deltaTime);
+            _duration -= deltaTime;
+            if (_duration <= 0)
+            {
+                stateMachine.SwitchState(new EnemyIdleState(stateMachine));
+            }
+        }
+    }
+}
---
+++ LegendsOfMaui/Assets/Scripts/StateMachines/Player/States/PlayerImapctState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlictronicGames.LegendsOfMaui.StateMachines.Player
+{
+    public class PlayerImapctState : PlayerBaseState
+    {
+        private readonly int IMPACT = Animator.StringToHash("Armed-GetHit-F2");
+        private const float ANIMATOR_DAMP_TIME = 0.1f;
+
+        private float _duration = 1f;
+
+        public PlayerImapctState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
+        {
+        }
+
+        public override void Enter()
+        {
+            stateMachine.Animator.CrossFadeInFixedTime(IMPACT, ANIMATOR_DAMP_TIME);
+        }
+
+        public override void Exit()
+        {
+            
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            Move(deltaTime);
+            _duration -= deltaTime;
+            if (_duration <= 0)
+            {
+                SwitchBackToLocmotion();
+            }
+        }
+    }
+}
