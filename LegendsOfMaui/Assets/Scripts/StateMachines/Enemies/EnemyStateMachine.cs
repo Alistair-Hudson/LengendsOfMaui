cac083246431e338c5bb0eea2656c8ccc9f472a5
AlistairHudson <alistairhudson64@gmail.com>
2023-08-01 10:58:39 +0300

    Created EnemyIdleState

--- LegendsOfMaui/Assets/Scripts/StateMachines/Enemies/EnemyStateMachine.cs
+++ LegendsOfMaui/Assets/Scripts/StateMachines/Enemies/EnemyStateMachine.cs
@@ -6,6 +6,16 @@ namespace AlictronicGames.LegendsOfMaui.StateMachines.Enemy
 {
     public class EnemyStateMachine : StateMachine
     {
+        public Animator Animator { get; private set; } = null;
 
+        private void Awake()
+        {
+            Animator = GetComponentInChildren<Animator>();
+        }
+
+        private void Start()
+        {
+            SwitchState(new EnemyIdleState(this));
+        }
     }
 }
---
+++ LegendsOfMaui/Assets/Scripts/StateMachines/Enemies/States/EnemyIdleState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlictronicGames.LegendsOfMaui.StateMachines.Enemy
+{
+    public class EnemyIdleState : EnemyBaseState
+    {
+        private readonly int FORWARD_SPEED = Animator.StringToHash("ForwardMovement");
+        private readonly int MOVEMENT = Animator.StringToHash("Movement");
+        private const float ANIMATOR_DAMP_TIME = 0.1f;
+
+        public EnemyIdleState(EnemyStateMachine enemyStateMachine) : base(enemyStateMachine)
+        {
+        }
+
+        public override void Enter()
+        {
+            _stateMachine.Animator.CrossFadeInFixedTime(MOVEMENT, ANIMATOR_DAMP_TIME);
+        }
+
+        public override void Exit()
+        {
+            
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            _stateMachine.Animator.SetFloat(FORWARD_SPEED, 0, ANIMATOR_DAMP_TIME, deltaTime);
+        }
+    }
+}
