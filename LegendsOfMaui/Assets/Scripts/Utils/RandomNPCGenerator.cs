1b9c5f59b623fd114120cb70ff0360699ed9d3f3
AlistairHudson <alistairhudson64@gmail.com>
2023-11-20 11:03:23 +0200

    Added NPC base. Model fixes required

---
+++ LegendsOfMaui/Assets/Scripts/Utils/RandomNPCGenerator.cs
@@ -0,0 +1,67 @@
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNPCGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] Heads;
+    [SerializeField]
+    GameObject[] HairStyles;
+    [SerializeField]
+    Material[] HairColours;
+    [SerializeField]
+    Material[] TaoMokos;
+
+    [Button("Generate NPC")]
+    void GenerateNPC()
+    {
+        HideGameObjects(Heads);
+        HideGameObjects(HairStyles);
+
+        int randomHeadIndex = UnityEngine.Random.Range(0, Heads.Length);
+        int randomHairStyleIndex = UnityEngine.Random.Range(0, HairStyles.Length);
+        int randomHairColourIndex = UnityEngine.Random.Range(0, HairColours.Length);
+        int randomTaoMokoIndex = UnityEngine.Random.Range(0, TaoMokos.Length);
+
+        GameObject head = Heads[randomHeadIndex];
+        head.SetActive(true);
+        if (head.TryGetComponent<MeshRenderer>(out var meshRender))
+        {
+            meshRender.material = TaoMokos[randomTaoMokoIndex];
+        }
+        else if (head.TryGetComponent<SkinnedMeshRenderer>(out var skinRender))
+        {
+            skinRender.material = TaoMokos[randomTaoMokoIndex];
+        }
+        else
+        {
+            Debug.LogError("No Mesh found for the Head");
+        }
+
+        GameObject hairstyle = HairStyles[randomHairStyleIndex];
+        hairstyle.SetActive(true);
+        if (hairstyle.TryGetComponent<MeshRenderer>(out var hairMeshRender))
+        {
+            hairMeshRender.material = HairColours[randomHairColourIndex];
+        }
+        else if (hairstyle.TryGetComponent<SkinnedMeshRenderer>(out var skinRender))
+        {
+            skinRender.material = HairColours[randomHairColourIndex];
+        }
+        else
+        {
+            Debug.LogError("No mesh found for the Hair");
+        }
+    }
+
+    private void HideGameObjects(GameObject[] gameObjects)
+    {
+        foreach (var item in gameObjects)
+        {
+            item.SetActive(false);
+        }
+    }
+}
