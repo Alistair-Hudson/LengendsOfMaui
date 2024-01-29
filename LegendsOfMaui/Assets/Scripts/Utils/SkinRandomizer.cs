e4d446c52426a95420f7ee5184c9fdb01cd3fdd5
AlistairHudson <alistairhudson64@gmail.com>
2024-01-29 11:13:55 +0200

    Created SkinRandomizer and added to Taniwha

---
+++ LegendsOfMaui/Assets/Scripts/Utils/SkinRandomizer.cs
@@ -0,0 +1,34 @@
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlictronicGames.LegendsOfMaui.Utils
+{
+    public class SkinRandomizer : SerializedMonoBehaviour
+    {
+        [SerializeField]
+        private string textureMapInput = "_BaseMap";
+        [OdinSerialize]
+        private Dictionary<Renderer, Texture[]> skinLUT = new Dictionary<Renderer, Texture[]>();
+
+        private void Awake()
+        {
+            foreach (Renderer renderer in skinLUT.Keys)
+            {
+                SelectandApplyRandomSkin(renderer);
+            }
+        }
+
+        [Button("Generate")]
+        private void SelectandApplyRandomSkin(Renderer renderer)
+        {
+            Material newMaterial = new Material(renderer.material);
+            int selectedIndex = UnityEngine.Random.Range(0, skinLUT[renderer].Length);
+            newMaterial.SetTexture(textureMapInput, skinLUT[renderer][selectedIndex]);
+            renderer.material = newMaterial;
+        }
+    }
+}
