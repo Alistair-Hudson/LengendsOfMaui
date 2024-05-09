6c7cde4b95cab00a4d071bf6e12228a6e65409eb
AlistairHudson <alistairhudson64@gmail.com>
2024-05-09 14:37:02 +0300

    work on creating the options menu, need to fix a button bug

--- LegendsOfMaui/Assets/Scripts/BackGroundSystems/PlayerOptions.cs
+++ LegendsOfMaui/Assets/Scripts/BackGroundSystems/PlayerOptions.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static TMPro.TMP_Dropdown;
 
 namespace AlictronicGames.LegendsOfMaui.BackGroundSystems
 {
@@ -19,21 +23,47 @@ namespace AlictronicGames.LegendsOfMaui.BackGroundSystems
             Maori
         }
 
+        [SerializeField]
+        private TMP_Dropdown screenResolutionDropdown = null;
+        [SerializeField]
+        private TMP_Dropdown screenModeDropdown = null;
+        [SerializeField]
+        private Slider volumeSlider = null;
+
         private float _globalVolume = 1f;
         private int _screenWidth = 1920;
         private int _screenHeight = 1080;
         private FullScreenMode _fullScreenMode = FullScreenMode.FullScreenWindow;
         private Language _language = Language.English;
 
-        private void LoadPlayerPrefs()
+        private void Awake()
         {
-            SetLanguage((Language)PlayerPrefs.GetInt(LANGUAGE, 0));
-            SetResolution(PlayerPrefs.GetInt(SCREEN_WIDTH, 1080), PlayerPrefs.GetInt(SCREEN_HEIGHT, 1920));
-            SetFullScreenMode((FullScreenMode)PlayerPrefs.GetInt(FULL_SCREEN_MODE, 1));
-            SetGlobalVolume(PlayerPrefs.GetFloat(GLOBAL_VOLUME, 1f));
+            screenResolutionDropdown.onValueChanged.AddListener(OnScreenResolutionChange);
+            screenModeDropdown.onValueChanged.AddListener(OnScreenModeChange);
+            volumeSlider.onValueChanged.AddListener(SetGlobalVolume);
         }
 
-        private void SavePlayerPrefs()
+        private void OnEnable()
+        {
+            LoadPlayerPrefs();
+        }
+
+        public void LoadPlayerPrefs()
+        {
+            string resolution = $"{PlayerPrefs.GetInt(SCREEN_WIDTH, 1920)}x{PlayerPrefs.GetInt(SCREEN_HEIGHT, 1080)}";
+            OptionData resultionData = new OptionData { text = resolution };
+            int resolutionValue = screenResolutionDropdown.options.IndexOf(resultionData);
+            screenResolutionDropdown.value =  resolutionValue;
+            screenModeDropdown.value = PlayerPrefs.GetInt(FULL_SCREEN_MODE, 0);
+            volumeSlider.value = PlayerPrefs.GetFloat(GLOBAL_VOLUME, 1f);
+
+            SetLanguage((Language)PlayerPrefs.GetInt(LANGUAGE, 0));
+            OnScreenResolutionChange(screenResolutionDropdown.value);
+            OnScreenModeChange(screenModeDropdown.value);
+            SetGlobalVolume(volumeSlider.value);
+        }
+
+        public void SavePlayerPrefs()
         {
             PlayerPrefs.SetInt(LANGUAGE, (int)_language);
             PlayerPrefs.SetInt(SCREEN_HEIGHT, _screenHeight);
@@ -64,6 +94,19 @@ namespace AlictronicGames.LegendsOfMaui.BackGroundSystems
         {
             _globalVolume = volume;
             AudioListener.volume = volume;
+        }
+
+        private void OnScreenResolutionChange(int dropdownValue)
+        {
+            string resolution = screenResolutionDropdown.options[dropdownValue].text;
+            string[] values = resolution.Split('x');
+            SetResolution(int.Parse(values[0]), int.Parse(values[1]));
+        }
+
+        private void OnScreenModeChange(int dropdownValue)
+        {
+            dropdownValue++; //this is here to remove the use of FullScreenMode.Exclussive
+            SetFullScreenMode((FullScreenMode)dropdownValue);
         }
     }
 }
