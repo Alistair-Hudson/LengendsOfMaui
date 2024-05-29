ccfae5ac062b243a15a30718942bd09f4b209659
AlistairHudson <alistairhudson64@gmail.com>
2024-05-29 16:18:10 +0300

    Upgrade to Unity 2023.2, added in river asset and a new mater asset

---
+++ LegendsOfMaui/Assets/Packages/WaterGeometryTools/RiverModeler/Editor/Asset.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace sc.modeling.river.editor
+{
+    public static class Asset
+    {
+        public const string NAME = "River Modeler";
+        public const string VERSION = "1.0.2";
+        public const int ID = 263898;
+
+        #region Installer
+        internal abstract class Installer : sc.modeling.water.common.editor.PackageInstaller
+        {
+            private static readonly string[] requiredPackages = 
+            {
+                "com.unity.splines@2.3.0", 
+                "com.unity.mathematics", 
+                "com.unity.visualeffectgraph",
+                "com.unity.shadergraph"
+            };
+            
+            //Symbols defined by assembly definition
+            #if !SPLINES || !MATHEMATICS || !VFXGRAPH || !SHADERGRAPH
+            [InitializeOnLoadMethod]
+            private static void AutoInstall()
+            {
+                InstallDependencies(NAME, requiredPackages);
+            }
+
+            [MenuItem("River Modeler/Install dependencies")]
+            private static void InstallMenuFunction()
+            {
+                AutoInstall();
+            }
+            #endif
+
+        }
+        #endregion
+        
+        public static class Preferences
+        {
+            public static bool ShowToolPoints
+            {
+                get => EditorPrefs.GetBool(PlayerSettings.productName + "_RIVER_EDITOR_ShowToolPoints", false);
+                private set => EditorPrefs.SetBool(PlayerSettings.productName + "_RIVER_EDITOR_ShowToolPoints", value);
+            }
+            
+            [SettingsProvider]
+            public static SettingsProvider ScreenshotSettings()
+            {
+                var provider = new SettingsProvider($"Preferences/Water Geometry Tools/River", SettingsScope.User)
+                {
+                    label = "River Editor",
+                    guiHandler = (searchContent) =>
+                    {
+                        ShowToolPoints = EditorGUILayout.ToggleLeft("Show tool points", ShowToolPoints);
+                        EditorGUILayout.HelpBox("When enabled, data points for tools such as Width and Opacity will always be displayed on rivers, even when not active.\n\n", MessageType.Info);
+                    },
+
+                    keywords = new HashSet<string>(new[]
+                    {
+                        "River"
+                    })
+                };
+
+                return provider;
+            }
+        }
+    }
+}
---
+++ LegendsOfMaui/Assets/Packages/WaterGeometryTools/RiverModeler/changelog.md
@@ -0,0 +1,20 @@
+﻿1.0.2
+
+Added:
+- Option under the UV section to store the rivers flow direction into the UV3 channel.
+
+Changed:
+- If a Mesh Collider is attached to the river, its mesh is now also updated
+
+1.0.1
+
+Added:
+- Support for mesh generation from multiple splines within a single container.
+- Automatic FBX file exporting and syncing.
+- VFX Graph: exposed curves for Size and Opacity over lifetime
+
+Fixed:
+- Error in inspector code that prevented a SplineContainer object from being dragged into the related field.
+
+1.0.0
+Initial release (March 4 2024)
