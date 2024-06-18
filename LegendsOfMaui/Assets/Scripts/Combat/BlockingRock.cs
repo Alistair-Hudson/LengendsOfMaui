9b663789f9bc4fa07a051c54af762c08fd82cfee
AlistairHudson <alistairhudson64@gmail.com>
2024-06-18 20:56:07 +0300

    Added a means of dealing with the case that Invulnerability can remain if rock is destroyed

--- LegendsOfMaui/Assets/Scripts/Combat/BlockingRock.cs
+++ LegendsOfMaui/Assets/Scripts/Combat/BlockingRock.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,45 @@ namespace AlictronicGames.LegendsOfMaui.Combat
 {
     public class BlockingRock : MonoBehaviour
     {
+        [SerializeField]
+        private float protectionDelay = 2f;
+
+        private List<Health> protectedHealths = new List<Health>();
+
+        private void OnDisable()
+        {
+            foreach (var health in protectedHealths)
+            {
+                health.StartCoroutine(DelayInvulnerabilityRemoval(health));
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<Health>(out Health health))
             {
                 health.SetInvulnerability(true);
+                protectedHealths.Add(health);
             }
         }
 
         private void OnTriggerExit(Collider other)
         {
-            if (other.TryGetComponent<Health>(out Health health))
+            if (!other.TryGetComponent<Health>(out Health health))
+            {
+                return;
+            }
+            if (protectedHealths.Contains(health))
             {
                 health.SetInvulnerability(false);
+                protectedHealths.Remove(health);
             }
+        }
+
+        private IEnumerator DelayInvulnerabilityRemoval(Health health)
+        {
+            yield return new WaitForSeconds(protectionDelay);
+            health.SetInvulnerability(false);
         }
     }
 }
