fc80ae812dcef3c8361fd97819718033131ef08d
AlistairHudson <alistairhudson64@gmail.com>
2024-06-26 22:50:34 +0300

    Fixed name display

--- LegendsOfMaui/Assets/Scripts/UI/NameDisplay.cs
+++ LegendsOfMaui/Assets/Scripts/UI/NameDisplay.cs
@@ -15,14 +15,7 @@ namespace AlictronicGames.LegendsOfMaui.UI
         {
             _nameDisplay = GetComponent<TMP_Text>();
             string name = GetComponentInParent<StateMachine>().gameObject.name;
-            for (int i = 1; i < name.Length; i++)
-            {
-                if (char.IsUpper(name[i]))
-                {
-                    name = name.Insert(i, " ");
-                    i++;
-                }   
-            }
+            name = name.Replace("(Clone)", "");
             _nameDisplay.text = name;
         }
     }
