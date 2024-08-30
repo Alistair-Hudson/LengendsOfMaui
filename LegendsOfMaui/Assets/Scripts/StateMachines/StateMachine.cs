a0e3c8f9ddcd6b74e31ffdc27b5d156524418989
Alistair Hudson <alistair.hudson@eyeviation.com>
2024-08-30 15:29:07 +0300

    added message to infor state changes for debugging

--- LegendsOfMaui/Assets/Scripts/StateMachines/StateMachine.cs
+++ LegendsOfMaui/Assets/Scripts/StateMachines/StateMachine.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace AlictronicGames.LegendsOfMaui.StateMachines
 {
-    public abstract class StateMachine : MonoBehaviour, IStateMachine
+    public abstract class StateMachine : SerializedMonoBehaviour, IStateMachine
     {
         private State _currentState = null;
 
@@ -24,6 +23,7 @@ namespace AlictronicGames.LegendsOfMaui.StateMachines
 
         public void SwitchState(State newState)
         {
+            Debug.Log($"{name} switching State {_currentState} -> {newState}");
             _currentState?.Exit();
             _currentState = newState;
             _currentState?.Enter();
