693eedde9c4d743f55c9f5ba99d9bae9894d8fef
Alistair Hudson <alistair.hudson@eyeviation.com>
2024-09-11 09:01:50 +0300

    Set up Taranga's Guradian with Attack Controller

---
+++ LegendsOfMaui/Assets/Scripts/Combat/BossAttacks/AttackControls/TarangasGuardianAttackController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using AlictronicGames.LegendsOfMaui.Combat;
+using UnityEngine;
+
+namespace AlictronicGames.LegendsOfMaui.Combat
+{
+    public class TarangasGuardianAttackController : BossAttackController
+    {
+
+    }
+}
